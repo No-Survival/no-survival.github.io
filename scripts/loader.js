4676bce6efcdd959cc93765cd8eb183ea3702b87
Björn Büttner <Idrinth@users.noreply.github.com>
2017-02-05 16:21:19 +0100

    Create loader.js

---
+++ scripts/loader.js
@@ -0,0 +1,14 @@
+(function(list) {
+var loader = function(){
+  var header=document.getElementsByTagName('head')[0];
+  for(var count=0;count<this.length;count++) {
+    var sc=document.createElement('script');
+    sc.setAttribute('src','/scripts/'+this[count]+'.js');
+    header.appendChild(sc);
+  }
+};
+loader.bind(list);
+document.addEventListener('load',loader);
+}([
+'base'
+]))
