66fc2314ca64e8cd389bae0c073cb1466f57d803
Björn Büttner <Idrinth@users.noreply.github.com>
2017-02-07 16:10:36 +0100

    adding label and button as default elements

--- scripts/resource.js
+++ scripts/resource.js
@@ -1,8 +1,9 @@
 var game=game?game:{};
 
-game.resource = function(name,startingValue,onTick) {
+game.resource = function(name,startingValue,onTick,onClick,label) {
   this.name = name;
   this.onTick = onTick?onTick:function(){};
+  this.onTick.bind(this);
   this.value = startingValue?startingValue:0;
   this.increase = function(amount) {
     this.value += amount?amount:1;
@@ -13,4 +14,13 @@ game.resource = function(name,startingValue,onTick) {
     }
     this.value -= amount;
   }
+  this.button = document.createElement('button');
+  this.button.innerHTML = label?label:name+"-action";
+  this.button.onclick = onClick?onClick:function(){};
+  this.label = document.createElement('div');
+  this.label.setAttribute('class','resource');
+  this.label.appendChild(document.createElement('div'));
+  this.label.lastChild.innerHTML=this.name;
+  this.label.appendChild(document.createElement('div'));
+  this.label.lastChild.innerHTML=this.value;
 }
