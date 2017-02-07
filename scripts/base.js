914367a4697dc630c315ad64944927150f638eef
Björn Büttner <Idrinth@users.noreply.github.com>
2017-02-07 08:59:57 +0100

    Update base.js

--- scripts/base.js
+++ scripts/base.js
@@ -1,1 +1,12 @@
-alert("Nothing done yet");
+function increase() {
+ if(gold > 100*Math.pow(1.1,house-1) {
+    gold-=100*Math.pow(1.1,house-1);
+    house++;
+    document.getElementById("gold").firstChild.innerHTML=gold;
+    document.getElementById("house").firstChild.innerHTML=house;
+ }
+}
+window.setInterval(function() {
+  gold+=0.1*(1+house);
+  document.getElementById("gold").firstChild.innerHTML=gold;
+},100);
