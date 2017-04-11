30ff3cd8bfbb5484e3e6a63fe4ac59c32556dd17
Björn Büttner <Idrinth@users.noreply.github.com>
2017-04-11 09:18:10 +0200

    Update game.js

--- scripts/game.js
+++ scripts/game.js
@@ -57,7 +57,10 @@ game.resource = function ( name, startingValue, onTick, onClick, label ) {
         this.button.innerHTML = label ? label : name + "-action";
         this.button.onclick = onClick;
         game.buttons.appendChild( this.button );
+    }  else {
+        this.button = document.createElement( 'div' );
     }
+    this.button.setAttribute('class','button');
     this.label = document.createElement( 'div' );
     this.label.setAttribute( 'class', 'resource' );
     this.label.appendChild( document.createElement( 'div' ) );
