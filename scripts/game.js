eb6c86216f5f694b0b22be09851a628159b050cf
Björn Büttner <Idrinth@users.noreply.github.com>
2017-04-13 11:43:35 +0200

    Update game.js

--- scripts/game.js
+++ scripts/game.js
@@ -1,5 +1,5 @@
 (function() {
-    var game = new require('game')(document.getElementById( 'stats' ));
+    var game = require('game')(document.getElementById( 'stats' ));
     game.tick.bind( game );
     game.interval = window.setInterval( game.tick, 100 );
     window.game = game;
