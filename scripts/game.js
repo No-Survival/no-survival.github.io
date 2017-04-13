e6415d1265740c9e18121a5c5ff4061412b5f52f
Björn Büttner <Idrinth@users.noreply.github.com>
2017-04-13 11:18:47 +0200

    Update game.js

--- scripts/game.js
+++ scripts/game.js
@@ -1,81 +1,87 @@
-var resource = require('resource');
-var modifier = require('modifier');
 var game = {
     resources: {},
     tick: function () {
         for ( var res in game.resources ) {
             game.resources[res].update();
         }
+    },
+    start: function() {
+        var makeResources = function() {
+            var resource = require('resource');
+            game.resources.gold = new resource(game.labels, 'gold', 50,
+                          function () {
+                              var tmp = 0.1 * Math.floor(game.resources.house.value) +
+                                  0.01 * Math.floor(game.resources.human.value)*(100000000+Math.floor(game.resources.minecart.value))/100000000 +
+                                  0.0001 - Math.ceil(game.resources.farm.value) * 0.01 -
+                                  Math.ceil(game.resources.minecart.value) * 0.025;
+                              game.resources["gold"].value += tmp;
+                              return tmp;
+                          },
+                          function () { game.resources.gold.increase( 1 + game.resources.minecart.value); },
+                          'Mine' );
+
+            game.resources.supplies = new resource(game.labels, 'supplies', 25, function () {
+                var tmp = Math.floor(game.resources.farm.value) * 0.1 - 0.01 * Math.ceil(game.resources.human.value)
+                game.resources.supplies.increase( tmp )
+                return tmp;
+            },
+                function () { game.resources.supplies.increase( 1+game.resources.farm.value ) }, 'Gather' );
+
+            game.resources.house = new resource( 'house', 0, function(event){
+                game.resources.house.increase( 0.00001 * Math.floor(game.resources.human.value-1) );
+                return 0.00001 * Math.floor(game.resources.human.value-1);
+            }, function ( event ) {
+                event = event || window.event;
+                if ( game.resources.gold.value > 100 * Math.pow( 1.1, game.resources.house.value ) ) {
+                    game.resources.gold.decrease( 100 * Math.pow( 1.1, game.resources.house.value ) );
+                    game.resources.house.increase( 1 );
+                }
+            }, 'Buy' );
+
+            game.resources.farm = new resource(game.labels, 'farm', 0, function(event){
+                game.resources.farm.increase( 0.00001 * Math.floor(game.resources.human.value-1) );
+                return 0.00001 * Math.floor(game.resources.human.value-1);
+            }, function ( event ) {
+                event = event || window.event;
+                if ( game.resources.gold.value > 100 * Math.pow( 1.1, game.resources.farm.value ) ) {
+                    game.resources.gold.decrease( 100 * Math.pow( 1.1, game.resources.farm.value ) );
+                    game.resources.farm.increase( 1 );
+                }
+            }, 'Buy' );
+
+            game.resources.minecart = new resource(game.labels, 'minecart', 0, function(event){
+                game.resources.minecart.increase( 0.000001 * Math.floor(game.resources.human.value-1) );
+                return 0.000001 * Math.floor(game.resources.human.value-1);
+            }, function ( event ) {
+                event = event || window.event;
+                if ( game.resources.gold.value > 100 * Math.pow( 1.1, game.resources.minecart.value ) ) {
+                    game.resources.gold.decrease( 100 * Math.pow( 1.1, game.resources.minecart.value ) );
+                    game.resources.minecart.increase( 1 );
+                }
+            }, 'Buy' );
+
+
+            game.resources.human = new resource(game.labels, 'human', 1, function () {
+                if ( game.resources['human'].value * 0.01 > game.resources["supplies"].value && game.resources['human'].value > 1 ) {
+                    game.resources.human.decrease( 0.01 * Math.ceil(game.resources['human'].value)*0.1 );
+                    return -0.01* game.resources['human'].value*0.1;
+                } else if ( game.resources['human'].value < game.resources['house'].value * 10 && game.resources['human'].value >= 2 ) {
+                    game.resources.human.increase( 0.01 * Math.floor(game.resources['human'].value)*0.1 );
+                    return 0.01* game.resources['human'].value*0.1;
+                }
+                return 0;
+            },function(event){
+                game.resources.human.increase( 0.0001 * game.resources.supplies.value );
+                game.resources.supplies.decrease( 0.0001 * game.resources.supplies.value );
+            }, 'Hire/Sacrifice' );
+        }
+        var modifier = require('modifier');
+        makeResources();
+        game.tick.bind( game );
+        game.buttons = document.getElementById( 'buttons' );
+        game.labels = document.getElementById( 'stats' );
+        game.interval = window.setInterval( game.tick, 100 );
+        delete game["start"];
     }
 };
-game.tick.bind( game );
-game.interval = window.setInterval( game.tick, 100 );
-game.buttons = document.getElementById( 'buttons' );
-game.labels = document.getElementById( 'stats' );
-
-game.resources.gold = new resource(game.labels, 'gold', 50,
-              function () {
-                  var tmp = 0.1 * Math.floor(game.resources.house.value) +
-                      0.01 * Math.floor(game.resources.human.value)*(100000000+Math.floor(game.resources.minecart.value))/100000000 +
-                      0.0001 - Math.ceil(game.resources.farm.value) * 0.01 -
-                      Math.ceil(game.resources.minecart.value) * 0.025;
-                  game.resources["gold"].value += tmp;
-                  return tmp;
-              },
-              function () { game.resources.gold.increase( 1 + game.resources.minecart.value); },
-              'Mine' );
-
-game.resources.supplies = new resource(game.labels, 'supplies', 25, function () {
-    var tmp = Math.floor(game.resources.farm.value) * 0.1 - 0.01 * Math.ceil(game.resources.human.value)
-    game.resources.supplies.increase( tmp )
-    return tmp;
-},
-    function () { game.resources.supplies.increase( 1+game.resources.farm.value ) }, 'Gather' );
-
-game.resources.house = new resource( 'house', 0, function(event){
-    game.resources.house.increase( 0.00001 * Math.floor(game.resources.human.value-1) );
-    return 0.00001 * Math.floor(game.resources.human.value-1);
-}, function ( event ) {
-    event = event || window.event;
-    if ( game.resources.gold.value > 100 * Math.pow( 1.1, game.resources.house.value ) ) {
-        game.resources.gold.decrease( 100 * Math.pow( 1.1, game.resources.house.value ) );
-        game.resources.house.increase( 1 );
-    }
-}, 'Buy' );
-
-game.resources.farm = new resource(game.labels, 'farm', 0, function(event){
-    game.resources.farm.increase( 0.00001 * Math.floor(game.resources.human.value-1) );
-    return 0.00001 * Math.floor(game.resources.human.value-1);
-}, function ( event ) {
-    event = event || window.event;
-    if ( game.resources.gold.value > 100 * Math.pow( 1.1, game.resources.farm.value ) ) {
-        game.resources.gold.decrease( 100 * Math.pow( 1.1, game.resources.farm.value ) );
-        game.resources.farm.increase( 1 );
-    }
-}, 'Buy' );
-
-game.resources.minecart = new resource(game.labels, 'minecart', 0, function(event){
-    game.resources.minecart.increase( 0.000001 * Math.floor(game.resources.human.value-1) );
-    return 0.000001 * Math.floor(game.resources.human.value-1);
-}, function ( event ) {
-    event = event || window.event;
-    if ( game.resources.gold.value > 100 * Math.pow( 1.1, game.resources.minecart.value ) ) {
-        game.resources.gold.decrease( 100 * Math.pow( 1.1, game.resources.minecart.value ) );
-        game.resources.minecart.increase( 1 );
-    }
-}, 'Buy' );
-
-
-game.resources.human = new resource(game.labels, 'human', 1, function () {
-    if ( game.resources['human'].value * 0.01 > game.resources["supplies"].value && game.resources['human'].value > 1 ) {
-        game.resources.human.decrease( 0.01 * Math.ceil(game.resources['human'].value)*0.1 );
-        return -0.01* game.resources['human'].value*0.1;
-    } else if ( game.resources['human'].value < game.resources['house'].value * 10 && game.resources['human'].value >= 2 ) {
-        game.resources.human.increase( 0.01 * Math.floor(game.resources['human'].value)*0.1 );
-        return 0.01* game.resources['human'].value*0.1;
-    }
-    return 0;
-},function(event){
-    game.resources.human.increase( 0.0001 * game.resources.supplies.value );
-    game.resources.supplies.decrease( 0.0001 * game.resources.supplies.value );
-}, 'Hire/Sacrifice' );
+game.start();
