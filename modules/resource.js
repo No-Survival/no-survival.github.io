55ebdb05064dfe1cf6345c40809b28c770b6e476
Björn Büttner <Idrinth@users.noreply.github.com>
2017-04-13 11:40:26 +0200

    Update resource.js

--- modules/resource.js
+++ modules/resource.js
@@ -1,4 +1,4 @@
-module = function ( element, name, startingValue, onTick, onClick, label ) {
+exports = function ( element, name, startingValue, onTick, onClick, label ) {
     this.name = name;
     this.onTick = onTick ? onTick : function () { return 0; };
     this.onTick.bind( this );
