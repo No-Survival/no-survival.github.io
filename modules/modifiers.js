f909213e245ed34c75d6cd349f39dcc2a35fe9d1
Björn Büttner <Idrinth@users.noreply.github.com>
2017-07-06 15:29:09 +0200

    Create modifiers.js

---
+++ modules/modifiers.js
@@ -0,0 +1,29 @@
+module.exports = function() {
+  this.modifiers = {};
+  this.add = function(modifier) {
+    this.modifiers[modifier.resource] = this.modifiers[modifier.resource]?this.modifiers[modifier.resource]:{};
+    this.modifiers[modifier.resource].early = this.modifiers[modifier.resource].early?this.modifiers[modifier.resource].early:[];
+    this.modifiers[modifier.resource].late = this.modifiers[modifier.resource].late?this.modifiers[modifier.resource].late:[];
+    this.modifiers[modifier.resource][modifier.early?'early':'late'].push(modifier);
+    this.modifiers[modifier.resource][modifier.early?'early':'late'].sort(function(a,b){return a.priority-b.priority;});
+  };
+  this.get = function (resource, base) {
+    var applyList = function(value,list) {
+      for(var e=list.length-1;e>-1;e--) {
+        if(list[e].multiply) {
+          value = value*list[e].value;
+        } else {
+          value = value+list[e].value;
+        }
+      }
+      return value;
+    }
+    if(!this.modifiers[resource]) {
+      return value;
+    }
+    return applyList(applyList(value,this.modifiers[resource].early),this.modifiers[resource].late);
+  };
+  this.clear = function() {
+    this.modifiers = {};
+  };
+};
