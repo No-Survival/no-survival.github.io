72fb0903de4098a051b3d0755d5b8a1098372160
Björn Büttner <Idrinth@users.noreply.github.com>
2017-07-06 15:50:11 +0200

    Update modifier.js

--- modules/modifier.js
+++ modules/modifier.js
@@ -4,4 +4,10 @@ module.exports = function(priority, early,value,isMultiply,resource) {
         this.resource = resource;
         this.priority = priority;
         this.early = early;
+        this.modify = funxtion(value) {
+                if(this.multiply) {
+                        return value*this.value;
+                }
+                return value.this.value;
+        }
 }
